d5fc53f211009a36596387f985beeda882ff5554
Paweł Morawczyński <pmorawczynski01@gmail.com>
2024-09-20 00:36:50 +0200

    project init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import "./reset.css";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -26,8 +27,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`text-accent-foreground ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+
         {children}
       </body>
     </html>
