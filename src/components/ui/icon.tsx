5a3d7cae3b71f3df28083b7053354a12c58774a2
Paweł Morawczyński <pmorawczynski01@gmail.com>
2024-09-21 15:27:55 +0200

    feat: add color theme, add button and icon component

---
+++ src/components/ui/icon.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { IconType } from 'react-icons';
+
+type IconProps = {
+  icon: IconType
+  className?: string,
+}
+
+function Icon(props: IconProps) {
+  const {className, icon: Component} = props;
+
+  return (
+    <Component
+      className={className}
+    />
+  );
+}
+
+export {Icon};
