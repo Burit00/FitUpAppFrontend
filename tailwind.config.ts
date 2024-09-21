d9a6b44ca0802782a6fced001a27a6281bf20235
Paweł Morawczyński <pmorawczynski01@gmail.com>
2024-09-21 15:26:31 +0200

    feat: update config, add shadcn-ui, add react-icons

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,12 +5,14 @@ const config: Config = {
     content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/containers/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
   	extend: {
   		colors: {
   			background: 'hsl(var(--background))',
+  			background2: 'hsl(var(--background2))',
   			foreground: 'hsl(var(--foreground))',
   			card: {
   				DEFAULT: 'hsl(var(--card))',
