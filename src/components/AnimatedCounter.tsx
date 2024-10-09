12eb211d3039bcf9a8eb0769434f70092baa410d
Paweł Morawczyński <pmorawczynski01@gmail.com>
2024-10-09 14:26:33 +0200

    refactor: AnimatedCounter.tsx

--- src/components/AnimatedCounter.tsx
+++ src/components/AnimatedCounter.tsx
@@ -7,23 +7,21 @@ type AnimatedCounterProps = {
 
 const AnimatedCounter = forwardRef<HTMLDivElement, AnimatedCounterProps>(
   ({ value, ...props }: AnimatedCounterProps, ref) => {
-    const valueStringArray = value.toString().split('');
+    const valueStringArray: string[] = value.toString().split('');
 
     return (
       <div {...props} ref={ref} className={cn('flex overflow-hidden', props.className)}>
-        {valueStringArray.map((char: string, index: number) => {
-          const oneHundred = 100;
-
+        {valueStringArray.map((digit: string, index: number) => {
           return (
             <div
               key={index}
-              style={{ translate: `0 -${parseInt(char) * oneHundred}%` }}
+              style={{ translate: `0 -${digit}00%` }}
               className={'relative transition-all ease-in-out duration-200 transform-gpu'}
             >
-              <span className={'opacity-0'}>{char}</span>
+              <span className={'opacity-0'}>{digit}</span>
               {Array.from({ length: 10 }).map((x, index) => {
                 return (
-                  <div key={index} style={{ top: `${index * oneHundred}%` }} className={'absolute'}>
+                  <div key={index} style={{ top: `${index}00%` }} className={'absolute'}>
                     {index}
                   </div>
                 );
