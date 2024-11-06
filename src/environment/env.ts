67344472fa93e23e9c57cb2e1610df7173e64406
Paweł Morawczyński <pmorawczynski01@gmail.com>
2024-11-06 17:02:12 +0100

    fix: public env keys mapped to env variable

--- src/environment/env.ts
+++ src/environment/env.ts
@@ -1,9 +1,13 @@
 import { z } from 'zod';
 
 const envSchema = z.object({
-  NEXT_PUBLIC_API_URL: z.string().url('API_URL must be a valid URL'),
+  API_URL: z.string().url('API_URL must be a valid URL'),
 });
 
 type TEnvironment = z.infer<typeof envSchema>;
 
-export const env: TEnvironment = envSchema.parse(process.env);
+const publicEnv: TEnvironment = {
+  API_URL: process.env.NEXT_PUBLIC_API_URL,
+};
+
+export const env: TEnvironment = envSchema.parse(publicEnv);
