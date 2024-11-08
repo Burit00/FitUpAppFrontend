1ee7eeba243d66bed209c1461bf23b87b752479b
Paweł Morawczyński <pmorawczynski01@gmail.com>
2024-11-08 16:16:51 +0100

    fix: conversion object to query params fixed

--- src/api/http/http-client.ts
+++ src/api/http/http-client.ts
@@ -124,7 +124,10 @@ export class HttpClient {
     const urlParams = new URLSearchParams();
 
     for (const [key, value] of Object.entries(params)) {
-      if (value) urlParams.append(key, value.toString());
+      if (!value) continue;
+      if (Array.isArray(value)) {
+        for (const v of value) urlParams.append(key, v.toString());
+      } else urlParams.append(key, value.toString());
     }
 
     return urlParams.toString();
