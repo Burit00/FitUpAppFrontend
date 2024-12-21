84673697d32479e93766737ee9836096ee515051
Paweł Morawczyński <burciak01@gmail.com>
2024-12-21 01:26:34 +0100

    feat: moved home page to /home

--- src/app/(auth)/_components/AuthGuard.tsx
+++ src/app/(auth)/_components/AuthGuard.tsx
@@ -3,12 +3,13 @@
 import { useRouter } from 'next/navigation';
 import { useCookie } from '@/hooks/useCookie';
 import { COOKIE_KEYS } from '@/constants/CookieKeys';
+import { HomePage } from '@/middleware';
 
 export default function AuthGuard() {
   const [token] = useCookie(COOKIE_KEYS.USER);
   const router = useRouter();
 
-  if (token) router.push('/');
+  if (token) router.push(HomePage);
 
   return null;
 }
