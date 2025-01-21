b60a5d1821e413d028f0167179066ea594eefd84
Paweł Morawczyński <burciak01@gmail.com>
2025-01-21 13:11:07 +0100

    fix: redirect to today's workout

--- src/app/(user)/workout/page.tsx
+++ src/app/(user)/workout/page.tsx
@@ -2,5 +2,7 @@ import { redirect } from 'next/navigation';
 import { toDateOnly } from '@/utils/date';
 
 export default function WorkoutPage() {
-  return redirect(`/workout/${toDateOnly(new Date())}`);
+  redirect(`/workout/${toDateOnly(new Date())}`);
+
+  return null;
 }
