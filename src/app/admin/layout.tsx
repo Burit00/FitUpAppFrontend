c213dd5ea38d365e543a13fbe7068bfd627a9b9b
Paweł Morawczyński <burciak01@gmail.com>
2025-01-25 14:10:43 +0100

    feat: admin navigation links added

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,6 +1,7 @@
 import NavigationLayout from '@components/layouts/NavigationLayout/NavigationLayout';
 import { AuthHeader } from '@features/auth/components/common/AuthHeader';
 import AuthGuard from '@/app/(user)/_components/AuthGuard';
+import { categories, exercises, users, settings } from '@/app/admin/links';
 
 type AdminLayoutProps = Readonly<{
   children: React.ReactNode;
@@ -11,7 +12,10 @@ export default function AdminLayout({ children }: AdminLayoutProps) {
     <>
       <AuthGuard/>
       <AuthHeader />
-      <NavigationLayout>{children}</NavigationLayout>
+      <NavigationLayout
+        topSectionLinks={[exercises, categories, users]}
+        bottomSectionLinks={[settings]}
+      >{children}</NavigationLayout>
     </>
   );
 }
